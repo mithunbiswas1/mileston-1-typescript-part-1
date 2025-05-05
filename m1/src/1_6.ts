bf699856e92a6dc6fa41473f01d050dde448a6fa
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-05-05 18:11:28 +0600

    dist setup

--- m1/src/1_6.ts
+++ m1/src/1_6.ts
@@ -1,5 +1,5 @@
 // Function 2 type
-// --> Normal Function
+// --> Normal Function (anonymous function)
 
 function add(num1: number, num2: number): number {
   return num1 + num2;
@@ -8,3 +8,18 @@ add(2, 2);
 
 // --> Arrow Function
 const addArrow = (num1: number = 10, num2: number = 12): number => num1 + num2;
+
+// Object --> function --> method
+// anonymous function (Normal Function)
+const pureUser = {
+  name: "Mezba",
+  balance: 0,
+  addBalance(balance: number): string {
+    return `My new balance is: ${this.balance + balance}`;
+  },
+};
+
+// map to retuen number squre
+
+const arr: number[] = [2, 4, 6];
+const newArray: number[] = arr.map((elem: number): number => elem * elem);
