9e54e5688b77f8aae6df33ee66dfa64b36b1818c
Mithun Biswas <mithunbiswas.me@gmail.com>
2025-05-21 10:38:26 +0600

    updates sfdf

---
+++ m3/3_3.ts
@@ -0,0 +1,46 @@
+{
+  // type guards
+
+  // typeof --> type gaurd
+
+  type AlphaNumeric = string | number;
+
+  const add = (param1: AlphaNumeric, param2: AlphaNumeric): AlphaNumeric => {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+      return param1 + param2;
+    } else {
+      return param1.toString() + param2.toString();
+    }
+  };
+  console.log(add("2", 3));
+
+  // in guard
+  type NormalUser = {
+    name: string;
+  };
+
+  type AdminUser = {
+    name: string;
+    role: string;
+  };
+
+  const getUser = (user: NormalUser | AdminUser) => {
+    if ("role" in user) {
+      console.log(`my name is ${user.name} and my role is ${user.role}`);
+    } else {
+      console.log(`my name is ${user.name} `);
+    }
+  };
+
+  const normalUser: NormalUser = {
+    name: "Mr Normal",
+  };
+  const adminUser: AdminUser = {
+    name: "Mr Normal",
+    role: "manager",
+  };
+
+  getUser(normalUser);
+
+  //
+}
